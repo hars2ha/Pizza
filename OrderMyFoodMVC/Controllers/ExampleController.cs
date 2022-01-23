5052cde443251e16e6afd8b073a47ea900bb7613
hars2ha <95631225+hars2ha@users.noreply.github.com>
2022-01-23 16:22:03 +0530

    first commit

---
+++ OrderMyFoodMVC/Controllers/ExampleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderMyFoodMVC.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Exampleone()
+        {
+            return View();
+        }
+
+        public ActionResult Exampletwo()
+        {
+
+            return View();
+        }
+        public ActionResult Examplethree()
+        {
+            return View();
+        }
+    }
+}
